e65f3d7308af27d6be7aa3d6772a7c5467d8a85d
Felipe Miquilini <felipe.miquilini@gmail.com>
2017-03-06 14:45:54 -0300

    Felipe

--- ViewHelper.php
+++ ViewHelper.php
@@ -23,6 +23,23 @@ class ViewHelper {
     return Text::join($opts2, '');
   }
 
+  public static function selectAttrOpt($attr, $text, $selected = false)  {
+    $option = "<option $attr";
+    $option .= $selected ? ' selected' : '';
+    $option .= ">$text</option>";
+    return $option;
+  }
+
+  public static function selectAttrOpts($array, $selected, $firstEmpty = true) {
+    $opts = Collection::map($array, function($a) use($selected) {
+      return static::selectAttrOpt($a[0], $a[1], $selected->isPresent() && $selected->get() == $a[0]);
+    });
+    $opts2 = $firstEmpty
+      ? Collection::prepend(static::selectAttrOpt('', 'SELECIONE'), $opts)
+      : $opts;
+    return Text::join($opts2, '');
+  }
+
   public static function checkbox($nome, $checked = false) {
     $checkbox = '<input type="checkbox" value="1" ';
     $checkbox .= "id=\"$nome\" ";
